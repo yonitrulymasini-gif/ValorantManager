a2b71b537f8547943a69eea97fe2bc59d2281cb8
yonitrulymasini-gif <yonitrulymasini@gmail.com>
2026-04-12 23:11:46 +0200

    Last Update

--- Properties/Resources.Designer.cs
+++ Properties/Resources.Designer.cs
@@ -8,7 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace RugbyManager.Properties {
+namespace ValorantManager.Properties {
     using System;
     
     
@@ -39,7 +39,7 @@ namespace RugbyManager.Properties {
         internal static global::System.Resources.ResourceManager ResourceManager {
             get {
                 if (object.ReferenceEquals(resourceMan, null)) {
-                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("RugbyManager.Properties.Resources", typeof(Resources).Assembly);
+                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("ValorantManager.Properties.Resources", typeof(Resources).Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;
