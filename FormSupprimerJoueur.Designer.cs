c9d6d197487dc7d353fa87c2fc73b01d002142cd
yonitrulymasini-gif <yonitrulymasini@gmail.com>
2026-04-12 13:30:02 +0200

    Finalizing the Valorant Manager application

--- FormSupprimerJoueur.Designer.cs
+++ FormSupprimerJoueur.Designer.cs
@@ -2,15 +2,8 @@
 {
     partial class FormSupprimerJoueur
     {
-        /// <summary>
-        /// Required designer variable.
-        /// </summary>
         private System.ComponentModel.IContainer components = null;
 
-        /// <summary>
-        /// Clean up any resources being used.
-        /// </summary>
-        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
         protected override void Dispose(bool disposing)
         {
             if (disposing && (components != null))
@@ -20,26 +13,201 @@
             base.Dispose(disposing);
         }
 
-        #region Windows Form Designer generated code
-
-        /// <summary>
-        /// Required method for Designer support - do not modify
-        /// the contents of this method with the code editor.
-        /// </summary>
         private void InitializeComponent()
         {
+            DataGridViewCellStyle dataGridViewCellStyle1 = new DataGridViewCellStyle();
+            DataGridViewCellStyle dataGridViewCellStyle2 = new DataGridViewCellStyle();
+            DataGridViewCellStyle dataGridViewCellStyle3 = new DataGridViewCellStyle();
+            DataGridViewCellStyle dataGridViewCellStyle4 = new DataGridViewCellStyle();
+            DataGridViewCellStyle dataGridViewCellStyle5 = new DataGridViewCellStyle();
+            DataGridViewCellStyle dataGridViewCellStyle6 = new DataGridViewCellStyle();
+            DataGridViewCellStyle dataGridViewCellStyle7 = new DataGridViewCellStyle();
+            DataGridViewCellStyle dataGridViewCellStyle8 = new DataGridViewCellStyle();
+            DataGridViewCellStyle dataGridViewCellStyle9 = new DataGridViewCellStyle();
+            DataGridViewCellStyle dataGridViewCellStyle10 = new DataGridViewCellStyle();
+            uiSmoothLabel1 = new Sunny.UI.UISmoothLabel();
+            uiButton1 = new Sunny.UI.UIButton();
+            uiButton6 = new Sunny.UI.UIButton();
+            uiDataGridView2 = new Sunny.UI.UIDataGridView();
+            uiDataGridView1 = new Sunny.UI.UIDataGridView();
+            ((System.ComponentModel.ISupportInitialize)uiDataGridView2).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)uiDataGridView1).BeginInit();
             SuspendLayout();
+            // 
+            // uiSmoothLabel1
+            // 
+            uiSmoothLabel1.Font = new Font("Microsoft Sans Serif", 72F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            uiSmoothLabel1.Location = new Point(613, 9);
+            uiSmoothLabel1.Name = "uiSmoothLabel1";
+            uiSmoothLabel1.RectSize = 15;
+            uiSmoothLabel1.Size = new Size(780, 125);
+            uiSmoothLabel1.TabIndex = 25;
+            uiSmoothLabel1.Text = "SUPPRIMER";
+            uiSmoothLabel1.Click += uiSmoothLabel1_Click;
+            // 
+            // uiButton1
+            // 
+            uiButton1.BackColor = SystemColors.ControlLight;
+            uiButton1.FillColor = Color.White;
+            uiButton1.FillColor2 = Color.White;
+            uiButton1.FillHoverColor = Color.DodgerBlue;
+            uiButton1.FillPressColor = Color.White;
+            uiButton1.FillSelectedColor = Color.White;
+            uiButton1.Font = new Font("Microsoft Sans Serif", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            uiButton1.ForeColor = Color.Black;
+            uiButton1.Location = new Point(840, 794);
+            uiButton1.MinimumSize = new Size(1, 1);
+            uiButton1.Name = "uiButton1";
+            uiButton1.RectColor = Color.White;
+            uiButton1.RectHoverColor = Color.FromArgb(224, 224, 224);
+            uiButton1.RectPressColor = Color.White;
+            uiButton1.RectSelectedColor = Color.White;
+            uiButton1.Size = new Size(293, 64);
+            uiButton1.TabIndex = 29;
+            uiButton1.Text = "SIMULER";
+            uiButton1.TipsColor = Color.White;
+            uiButton1.TipsFont = new Font("Microsoft Sans Serif", 9F);
+            uiButton1.Click += uiButton1_Click;
+            // 
+            // uiButton6
+            // 
+            uiButton6.BackColor = SystemColors.ControlLight;
+            uiButton6.FillColor = Color.White;
+            uiButton6.FillColor2 = Color.White;
+            uiButton6.FillHoverColor = Color.DodgerBlue;
+            uiButton6.FillPressColor = Color.White;
+            uiButton6.FillSelectedColor = Color.White;
+            uiButton6.Font = new Font("Microsoft Sans Serif", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            uiButton6.ForeColor = Color.Black;
+            uiButton6.Location = new Point(840, 878);
+            uiButton6.MinimumSize = new Size(1, 1);
+            uiButton6.Name = "uiButton6";
+            uiButton6.RectColor = Color.White;
+            uiButton6.RectHoverColor = Color.FromArgb(224, 224, 224);
+            uiButton6.RectPressColor = Color.White;
+            uiButton6.RectSelectedColor = Color.White;
+            uiButton6.Size = new Size(293, 64);
+            uiButton6.TabIndex = 28;
+            uiButton6.Text = "RETOUR";
+            uiButton6.TipsColor = Color.White;
+            uiButton6.TipsFont = new Font("Microsoft Sans Serif", 9F);
+            uiButton6.Click += uiButton6_Click;
+            // 
+            // uiDataGridView2
+            // 
+            dataGridViewCellStyle1.BackColor = Color.FromArgb(235, 243, 255);
+            uiDataGridView2.AlternatingRowsDefaultCellStyle = dataGridViewCellStyle1;
+            uiDataGridView2.BackgroundColor = Color.White;
+            uiDataGridView2.ColumnHeadersBorderStyle = DataGridViewHeaderBorderStyle.Single;
+            dataGridViewCellStyle2.Alignment = DataGridViewContentAlignment.MiddleCenter;
+            dataGridViewCellStyle2.BackColor = Color.FromArgb(80, 160, 255);
+            dataGridViewCellStyle2.Font = new Font("Microsoft Sans Serif", 12F);
+            dataGridViewCellStyle2.ForeColor = Color.White;
+            dataGridViewCellStyle2.SelectionBackColor = SystemColors.Highlight;
+            dataGridViewCellStyle2.SelectionForeColor = SystemColors.HighlightText;
+            dataGridViewCellStyle2.WrapMode = DataGridViewTriState.True;
+            uiDataGridView2.ColumnHeadersDefaultCellStyle = dataGridViewCellStyle2;
+            uiDataGridView2.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridViewCellStyle3.Alignment = DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle3.BackColor = SystemColors.Window;
+            dataGridViewCellStyle3.Font = new Font("Microsoft Sans Serif", 12F);
+            dataGridViewCellStyle3.ForeColor = SystemColors.ControlText;
+            dataGridViewCellStyle3.SelectionBackColor = SystemColors.Highlight;
+            dataGridViewCellStyle3.SelectionForeColor = SystemColors.HighlightText;
+            dataGridViewCellStyle3.WrapMode = DataGridViewTriState.False;
+            uiDataGridView2.DefaultCellStyle = dataGridViewCellStyle3;
+            uiDataGridView2.EnableHeadersVisualStyles = false;
+            uiDataGridView2.Font = new Font("Microsoft Sans Serif", 12F);
+            uiDataGridView2.GridColor = Color.FromArgb(80, 160, 255);
+            uiDataGridView2.Location = new Point(772, 22);
+            uiDataGridView2.Name = "uiDataGridView2";
+            uiDataGridView2.RowHeadersBorderStyle = DataGridViewHeaderBorderStyle.Single;
+            dataGridViewCellStyle4.Alignment = DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle4.BackColor = Color.FromArgb(235, 243, 255);
+            dataGridViewCellStyle4.Font = new Font("Microsoft Sans Serif", 12F);
+            dataGridViewCellStyle4.ForeColor = Color.FromArgb(48, 48, 48);
+            dataGridViewCellStyle4.SelectionBackColor = Color.FromArgb(80, 160, 255);
+            dataGridViewCellStyle4.SelectionForeColor = Color.White;
+            dataGridViewCellStyle4.WrapMode = DataGridViewTriState.True;
+            uiDataGridView2.RowHeadersDefaultCellStyle = dataGridViewCellStyle4;
+            uiDataGridView2.RowHeadersWidth = 51;
+            dataGridViewCellStyle5.BackColor = Color.White;
+            dataGridViewCellStyle5.Font = new Font("Microsoft Sans Serif", 12F);
+            uiDataGridView2.RowsDefaultCellStyle = dataGridViewCellStyle5;
+            uiDataGridView2.SelectedIndex = -1;
+            uiDataGridView2.Size = new Size(10, 8);
+            uiDataGridView2.StripeOddColor = Color.FromArgb(235, 243, 255);
+            uiDataGridView2.TabIndex = 31;
+            // 
+            // uiDataGridView1
+            // 
+            dataGridViewCellStyle6.BackColor = Color.FromArgb(235, 243, 255);
+            uiDataGridView1.AlternatingRowsDefaultCellStyle = dataGridViewCellStyle6;
+            uiDataGridView1.BackgroundColor = Color.White;
+            uiDataGridView1.ColumnHeadersBorderStyle = DataGridViewHeaderBorderStyle.Single;
+            dataGridViewCellStyle7.Alignment = DataGridViewContentAlignment.MiddleCenter;
+            dataGridViewCellStyle7.BackColor = Color.FromArgb(80, 160, 255);
+            dataGridViewCellStyle7.Font = new Font("Microsoft Sans Serif", 12F);
+            dataGridViewCellStyle7.ForeColor = Color.White;
+            dataGridViewCellStyle7.SelectionBackColor = SystemColors.Highlight;
+            dataGridViewCellStyle7.SelectionForeColor = SystemColors.HighlightText;
+            dataGridViewCellStyle7.WrapMode = DataGridViewTriState.True;
+            uiDataGridView1.ColumnHeadersDefaultCellStyle = dataGridViewCellStyle7;
+            uiDataGridView1.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridViewCellStyle8.Alignment = DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle8.BackColor = SystemColors.Window;
+            dataGridViewCellStyle8.Font = new Font("Microsoft Sans Serif", 12F);
+            dataGridViewCellStyle8.ForeColor = SystemColors.ControlText;
+            dataGridViewCellStyle8.SelectionBackColor = SystemColors.Highlight;
+            dataGridViewCellStyle8.SelectionForeColor = SystemColors.HighlightText;
+            dataGridViewCellStyle8.WrapMode = DataGridViewTriState.False;
+            uiDataGridView1.DefaultCellStyle = dataGridViewCellStyle8;
+            uiDataGridView1.EnableHeadersVisualStyles = false;
+            uiDataGridView1.Font = new Font("Microsoft Sans Serif", 12F);
+            uiDataGridView1.GridColor = Color.FromArgb(80, 160, 255);
+            uiDataGridView1.Location = new Point(468, 156);
+            uiDataGridView1.Name = "uiDataGridView1";
+            uiDataGridView1.RowHeadersBorderStyle = DataGridViewHeaderBorderStyle.Single;
+            dataGridViewCellStyle9.Alignment = DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle9.BackColor = Color.FromArgb(235, 243, 255);
+            dataGridViewCellStyle9.Font = new Font("Microsoft Sans Serif", 12F);
+            dataGridViewCellStyle9.ForeColor = Color.FromArgb(48, 48, 48);
+            dataGridViewCellStyle9.SelectionBackColor = Color.FromArgb(80, 160, 255);
+            dataGridViewCellStyle9.SelectionForeColor = Color.White;
+            dataGridViewCellStyle9.WrapMode = DataGridViewTriState.True;
+            uiDataGridView1.RowHeadersDefaultCellStyle = dataGridViewCellStyle9;
+            uiDataGridView1.RowHeadersWidth = 51;
+            dataGridViewCellStyle10.BackColor = Color.White;
+            dataGridViewCellStyle10.Font = new Font("Microsoft Sans Serif", 12F);
+            uiDataGridView1.RowsDefaultCellStyle = dataGridViewCellStyle10;
+            uiDataGridView1.SelectedIndex = -1;
+            uiDataGridView1.Size = new Size(1053, 595);
+            uiDataGridView1.StripeOddColor = Color.FromArgb(235, 243, 255);
+            uiDataGridView1.TabIndex = 32;
             // 
             // FormSupprimerJoueur
             // 
-            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleDimensions = new SizeF(8F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(800, 450);
+            ClientSize = new Size(1902, 1033);
+            Controls.Add(uiDataGridView1);
+            Controls.Add(uiDataGridView2);
+            Controls.Add(uiButton1);
+            Controls.Add(uiButton6);
+            Controls.Add(uiSmoothLabel1);
+            Margin = new Padding(3, 4, 3, 4);
             Name = "FormSupprimerJoueur";
-            Text = "<";
+            Text = "Supprimer un joueur";
+            Load += FormSupprimerJoueur_Load;
+            ((System.ComponentModel.ISupportInitialize)uiDataGridView2).EndInit();
+            ((System.ComponentModel.ISupportInitialize)uiDataGridView1).EndInit();
             ResumeLayout(false);
         }
 
-        #endregion
+        private Sunny.UI.UISmoothLabel uiSmoothLabel1;
+        private Sunny.UI.UIButton uiButton1;
+        private Sunny.UI.UIButton uiButton6;
+        private Sunny.UI.UIDataGridView uiDataGridView2;
+        private Sunny.UI.UIDataGridView uiDataGridView1;
     }
 }
--- FormSupprimerJoueur.cs
+++ FormSupprimerJoueur.cs
@@ -1,18 +1,172 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
 using System.Data;
-using System.Drawing;
-using System.Text;
 using System.Windows.Forms;
+using MySql.Data.MySqlClient;
 
 namespace RugbyManager
 {
     public partial class FormSupprimerJoueur : Form
     {
+        string connectionString = "Server=127.0.0.1;Port=3306;Database=valorant_manager;Uid=root;Pwd=root;";
+
         public FormSupprimerJoueur()
         {
             InitializeComponent();
+            this.Load += FormSupprimerJoueur_Load;
+            this.WindowState = FormWindowState.Maximized;
+        }
+
+        private void FormSupprimerJoueur_Load(object sender, EventArgs e)
+        {
+            uiDataGridView1.SelectionMode = DataGridViewSelectionMode.FullRowSelect;
+            uiDataGridView1.MultiSelect = false;
+            uiDataGridView1.ReadOnly = true;
+            uiDataGridView1.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.Fill;
+
+            AfficherJoueurs();
+        }
+
+        private void AfficherJoueurs()
+        {
+            using (MySqlConnection conn = new MySqlConnection(connectionString))
+            {
+                try
+                {
+                    conn.Open();
+                    MySqlCommand cmd = new MySqlCommand(@"
+                        SELECT j.id, j.nom, e.nom AS equipe, j.poste
+                        FROM Joueurs j
+                        LEFT JOIN Equipes e ON j.equipe_id = e.id
+                        ORDER BY j.nom", conn);
+
+                    MySqlDataReader reader = cmd.ExecuteReader();
+                    DataTable dt = new DataTable();
+                    dt.Columns.Add("ID");
+                    dt.Columns.Add("Nom");
+                    dt.Columns.Add("Equipe");
+                    dt.Columns.Add("Poste");
+
+                    while (reader.Read())
+                    {
+                        dt.Rows.Add(
+                            reader.GetInt32("id"),
+                            reader.GetString("nom"),
+                            reader.IsDBNull(2) ? "Sans équipe" : reader.GetString(2),
+                            reader.GetString("poste")
+                        );
+                    }
+
+                    uiDataGridView1.DataSource = dt;
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Erreur : " + ex.Message, "Erreur",
+                        MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+        }
+
+        private void btnSupprimer_Click(object sender, EventArgs e)
+        {
+            if (uiDataGridView1.SelectedRows.Count == 0)
+            {
+                MessageBox.Show("Veuillez sélectionner un joueur à supprimer !", "Erreur",
+                    MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+
+            int id = Convert.ToInt32(uiDataGridView1.SelectedRows[0].Cells["ID"].Value);
+            string nom = uiDataGridView1.SelectedRows[0].Cells["Nom"].Value.ToString();
+
+            DialogResult confirm = MessageBox.Show(
+                $"Voulez-vous vraiment supprimer le joueur '{nom}' ?",
+                "Confirmation", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+
+            if (confirm == DialogResult.Yes)
+            {
+                using (MySqlConnection conn = new MySqlConnection(connectionString))
+                {
+                    try
+                    {
+                        conn.Open();
+                        MySqlCommand cmd = new MySqlCommand(
+                            "DELETE FROM Joueurs WHERE id = @id", conn);
+                        cmd.Parameters.AddWithValue("@id", id);
+                        cmd.ExecuteNonQuery();
+
+                        MessageBox.Show("Joueur supprimé !", "Succès",
+                            MessageBoxButtons.OK, MessageBoxIcon.Information);
+
+                        AfficherJoueurs();
+                    }
+                    catch (Exception ex)
+                    {
+                        MessageBox.Show("Erreur : " + ex.Message, "Erreur",
+                            MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    }
+                }
+            }
+        }
+
+        private void btnRetour_Click(object sender, EventArgs e)
+        {
+            FormAccueil formMain = new FormAccueil();
+            formMain.Show();
+            this.Close();
+        }
+
+        private void uiSmoothLabel1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void uiButton6_Click(object sender, EventArgs e)
+        {
+            FormAccueil formMain = new FormAccueil();
+            formMain.Show();
+            this.Close();
+        }
+
+        private void uiButton1_Click(object sender, EventArgs e)
+        {
+            if (uiDataGridView1.SelectedRows.Count == 0)
+            {
+                MessageBox.Show("Veuillez sélectionner un joueur à supprimer !", "Erreur",
+                    MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+
+            int id = Convert.ToInt32(uiDataGridView1.SelectedRows[0].Cells["ID"].Value);
+            string nom = uiDataGridView1.SelectedRows[0].Cells["Nom"].Value.ToString();
+
+            DialogResult confirm = MessageBox.Show(
+                $"Voulez-vous vraiment supprimer le joueur '{nom}' ?",
+                "Confirmation", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+
+            if (confirm == DialogResult.Yes)
+            {
+                using (MySqlConnection conn = new MySqlConnection(connectionString))
+                {
+                    try
+                    {
+                        conn.Open();
+                        MySqlCommand cmd = new MySqlCommand(
+                            "DELETE FROM Joueurs WHERE id = @id", conn);
+                        cmd.Parameters.AddWithValue("@id", id);
+                        cmd.ExecuteNonQuery();
+
+                        MessageBox.Show("Joueur supprimé !", "Succès",
+                            MessageBoxButtons.OK, MessageBoxIcon.Information);
+
+                        AfficherJoueurs();
+                    }
+                    catch (Exception ex)
+                    {
+                        MessageBox.Show("Erreur : " + ex.Message, "Erreur",
+                            MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    }
+                }
+            }
         }
     }
 }
